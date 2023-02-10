ab29cf5e3139115691a23ff4220613a292b05dfe
naoking0623 <19800623naoki@gmail.com>
2023-02-10 13:28:31 +0000

    laravel10

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>風呂フィル2</title>
+    </head>
+    <body>
+        <h1>今度は出口</h1>
+        <2>今日はおしまい。</2>
+    </body>
+</html>
