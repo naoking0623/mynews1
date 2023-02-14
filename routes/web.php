3afbfd28fff578ace4b04a35032cc4f76e9f66df
naoking0623 <19800623naoki@gmail.com>
2023-02-14 12:19:12 +0000

    PHP/Laravel 12

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,14 @@ Route::controller(NewsController::class)->prefix('admin')->group(function() {
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add')->middleware('auth');
+});
