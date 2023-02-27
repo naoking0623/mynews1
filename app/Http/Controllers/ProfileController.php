474031598e8c7fb67dc9ca9c2e0d3944aab61141
naoking0623 <19800623naoki@gmail.com>
2023-02-27 12:52:02 +0000

    testo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,22 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 // 追記
-use App\Models\ProfileHistory;
+use App\Models\Profile;
 
 class ProfileController extends Controller
 {
     public function index(Request $request)
     {
-        $posts = ProfileHistory::all()->sortByDesc('updated_at');
-
-        if (count($posts) > 0) {
-            $headline = $posts->shift();
-        } else {
-            $headline = null;
-        }
+        $posts = Profile::all()->sortByDesc('updated_at');
 
         // news/index.blade.php ファイルを渡している
         // また View テンプレートに headline、 posts、という変数を渡している
-        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
+        return view('profile.index', ['posts' => $posts]);
     }//
 }
