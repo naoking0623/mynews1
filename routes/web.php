33eced2cffb7f5e423400656874a64ecec06b78b
naoking0623 <19800623naoki@gmail.com>
2023-03-02 12:28:54 +0000

    hennko1

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 use App\Http\Controllers\NewsController as PublicNewsController;
 Route::get('/', [PublicNewsController::class, 'index'])->name('news.index');
 
-use App\Http\Controllers\ProfileController as PublicProfileController;
-Route::get('/', [PublicProfileController::class, 'index'])->name('profile.index');
+
+
+
 
 
